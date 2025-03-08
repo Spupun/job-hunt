737e0b5453ccfeddf1e078a66e464b9f09c6bd14
Mohan Subudhi <mohansubudhi453@gmail.com>
2025-03-09 00:13:17 +0530

    fixed the login issue

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -93,7 +93,7 @@ export const login = async (req, res) => {
     // Generate a JWT token
     const token = jwt.sign(
       { userId: user._id, role: user.role },
-      process.env.JWT_SECRET_KEY,
+      process.env.SECRET_KEY,
       { expiresIn: "1d" }
     );
 
